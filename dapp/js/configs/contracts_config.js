7ae29ba063e07e319687ddbbe3e7f3077fee77de
sadam <sadamccr@gmail.com>
2023-02-19 18:55:34 +0500

    json files of contract moved to CONTRACTS Folder

---
+++ dapp/js/configs/contracts_config.js
@@ -0,0 +1,12 @@
+let electionCommissionContractAddress = "";
+let ec_contract = "";
+let candidateContract = "";
+let voterContract = "";
+
+let ecObject = null;
+let candidateObject;
+let voterObject;
+
+const initializeMainContract = async(web3)=>{
+    
+}
