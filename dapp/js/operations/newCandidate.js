6041fce5bf925f810d4626350dc13ab02a3732a3
sadam <sadamccr@gmail.com>
2023-02-28 02:19:34 +0500

    newCandidate.js file added & linked with its html

---
+++ dapp/js/operations/newCandidate.js
@@ -0,0 +1,11 @@
+function onSubmitForm() {
+    var fname = $("#fname").val();
+    var lname = $("#lname").val();
+    var party = $("#party").val();
+    var symbol = $("#symbol").val();
+    var ec_address = $("#ec_address").val();
+
+    console.log("fname = "+ fname + " " + "lname = " + lname+ " "
+    + "party = " + party + " " + "symbol = " + symbol + " " + "address = " + ec_address);
+
+}
